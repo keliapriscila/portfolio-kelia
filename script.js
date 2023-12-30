c65e8fba53ae211e911c5a589d80433b5648e21a
Kélia Priscila Santos do Amaral <keliapriscila@hotmail.com>
2023-12-30 11:15:39 -0300

    Lista de tarefas to do list
    Lista de tarefas feita em html, css, javascript.

---
+++ script.js
@@ -0,0 +1,72 @@
+const button = document.querySelector('.add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-task')
+
+let minhaListaDeItens = []
+
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push({
+        tarefa : input.value,
+        concluida: false 
+    })
+
+     
+    mostrarTarefas()
+
+}
+
+
+
+
+function mostrarTarefas() {
+
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((item, posicao) => {
+
+        novaLi = novaLi + `
+
+        <li class="task ${item.concluida && "done"}">
+                <img src="./img/checked.png" alt="Checar tarefa" onclick="concluirTarefa(${posicao})">
+                <p>${item.tarefa}</p>
+                <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarTarefa(${posicao})">
+            </li>
+        
+        
+        `
+
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem('lista',JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao){
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
+
+}
+
+function deletarTarefa(posicao){
+    minhaListaDeItens.splice(posicao,1)
+    
+    mostrarTarefas()
+}
+
+function recarregarTarefas(){
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+    if(tarefasDoLocalStorage){
+        minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    }
+    
+
+    mostrarTarefas()
+}
+
+
+recarregarTarefas()
+button.addEventListener('click', adicionarNovaTarefa)
